9581f1f869205bf787c544dc20fffeabf8cf8098
I355539 <nitai.aharoni@sap.com>
2019-03-31 10:36:52 +0300

    init

--- ex1.js
+++ ex1.js
@@ -1,116 +1,6 @@
-var q1 = function(){
-    var doSomething = function(){
-        var acc = 0
-        var show_time = false;
-        var timerID = null;
-        var blank = ":";
-
-        function stopit(){
-            if(show_time)
-                clearTimeout(timerID);
-            show_time = false;
-        }
-
-        function startit(form){
-            var today = new Date();
-            var curyear = today.getYear()
-            if(curyear < 1000)
-                curyear += 1900
-            var acc1 = acc;
-            var display_value = " Time:  " + curyear
-            if(today.getMinutes() < 10){
-                display_value += ":0" + today.getMinutes();
-            } else{
-                display_value += ":" + today.getMinutes();
-            }
-            if(today.getSeconds() < 10){
-                display_value += ":0" + today.getSeconds();
-            } else{
-                display_value += ":" + today.getSeconds();
-            }
-            if
-            (today.getHours() >= 12){
-                display_value += " P.M.  "
-            } else{
-                display_value += " A.M.  "
-            }
-            display_value += " Date:  " + (today.getMonth() + 1) + "/" + today.getDate() + "/" + curyear;
-            window.status = display_value;
-            writeyourAge(acc1)
-            timerID = setTimeout("startit()", 100);
-            show_time = true;
-        }
-
-        function writeyourAge(onval){
-            if(onval == 0){
-                normal()
-            } else{
-                accurate()
-            }
-        }
-
-        function normal(){
-            TheDate = new Date();
-            Month = TheDate.getMonth();
-            Day = TheDate.getDate();
-            Year = TheDate.getYear();
-            if(Year < 1000)
-                Year += 1900
-            Hour = TheDate.getHours();
-            Minute = TheDate.getMinutes();
-            Second = TheDate.getSeconds();
-            by = (document.my_age.birthy.value);
-            bm = (document.my_age.birthm.value);
-            bd = (document.my_age.birthd.value);
-            bh = (document.my_age.birthh.value);
-            bmn = (document.my_age.birthmin.value);
-            bs = (document.my_age.births.value);
-            BDate = Date.UTC(by, bm, bd, bh, bmn, bs);
-            if(Year < 2000){
-                CDate = Date.UTC(Year, Month, Day, Hour, Minute, Second);
-            } else{
-                CDate = Date.UTC(Year, Month, Day, Hour, Minute, Second);
-            }
-            Age = CDate - BDate + (1000 * 60 * 60 * 24 * 30);
-            document.my_age.yrs.value = parseInt(((((Age / 1000) / 60) / 60) / 24) / 365.25, 10);
-            document.my_age.dys.value = parseInt((((Age / 1000) / 60) / 60) / 24, 10);
-            document.my_age.hrs.value = parseInt(((Age / 1000) / 60) / 60, 10);
-            document.my_age.mins.value = parseInt((Age / 1000) / 60, 10);
-            document.my_age.secs.value = parseInt(Age / 1000, 10);
-        }
-
-        function accurate(){
-            TheDate = new Date();
-            Month = TheDate.getMonth();
-            Day = TheDate.getDate();
-            Year = TheDate.getYear();
-            if(Year < 1000)
-                Year += 1900
-            Hour = TheDate.getHours();
-            Minute = TheDate.getMinutes();
-            Second = TheDate.getSeconds();
-            by = (document.my_age.birthy.value);
-            bm = (document.my_age.birthm.value);
-            bd = (document.my_age.birthd.value);
-            bh = (document.my_age.birthh.value);
-            bmn = (document.my_age.birthmin.value);
-            bs = (document.my_age.births.value);
-            BDate = Date.UTC(by, bm, bd, bh, bmn, bs);
-            if(Year < 2000){
-                CDate = Date.UTC(Year, Month, Day, Hour, Minute, Second);
-            } else{
-                CDate = Date.UTC(Year, Month, Day, Hour, Minute, Second);
-            }
-            Age = CDate - BDate + (1000 * 60 * 60 * 24 * 30);
-            document.my_age.yrs.value = ((((Age / 1000) / 60) / 60) / 24) / 365.25;
-            document.my_age.dys.value = (((Age / 1000) / 60) / 60) / 24;
-            document.my_age.hrs.value = ((Age / 1000) / 60) / 60;
-            document.my_age.mins.value = (Age / 1000) / 60;
-            document.my_age.secs.value = Age / 1000;
-        }
-
+var q1 = function () {
+    var doSomething = function () {
     };
-
     var desc = "string : a description of what doSomething() is doing and why let changes it";
     return {
         "doSomething": doSomething,
@@ -118,13 +8,11 @@ var q1 = function(){
     }
 };
 
-var q2 = function(){
-    var doSomething = function(){
-        //function: do something that uses only ‘var’s and would behave completely differently if you would use ‘let’
-        // instead, but it should return correctly
+var q2 = function () {
+    var doSomething = function () {
         var i = 5;
         console.log("Start: i = " + i);
-        for(var i = 0; i < 100; i++){
+        for (var i = 0; i < 100; i++) {
             console.log("Loop: i = " + i);
         }
         console.log("Finish: i = " + i);
@@ -137,50 +25,55 @@ var q2 = function(){
     }
 };
 
-function q3(){
-    //function: return an array of 10 ( a to k) objects that structured as the following prototype-chain
-    // a->b->c->d->e->f->g->i->j->k
-    // string: description of what doSomething() is doing
-    var ObjectFactory = function(func, desc){
-        var obj = {};
-        obj.doSomething = function() {
-            var array = [];
-            this.array.push();
+function q3() {
+    function ObjectFactory(func, desc) {
+        return {
+            doSomething: function () {
+                var k = {k: 1};
+                var j = Object.create(k);
+                var i = Object.create(j);
+                var h = Object.create(i);
+                var g = Object.create(h);
+                var f = Object.create(g);
+                var e = Object.create(f);
+                var d = Object.create(e);
+                var c = Object.create(d);
+                var b = Object.create(c);
+                var a = Object.create(b);
+                var array = [a, b, c, d, e, f, g, h, i, j, k];
+                return array;
+            },
+
+            desc: "doSomething returns an array of 10 empty objects (a to k) that are structured as the following prototype-chain a->b->c->d->e->f->g->i->j->k"
+        }
+    };
+
+    return new ObjectFactory();
+}
+
+function q4() {
+    var ObjectConstructor = function () {
+        this.doSomething = function () {
+            var Calculator = function () {
+                var _num = 0;
+
+                this.add = function (num) {
+                    _num += num;
+                };
+
+                this.sub = function (num) {
+                    _num -= num;
+                };
+
+                this.getValue = function () {
+                    return _num;
+                }
+            };
+            return new Calculator();
         };
-        obj.desc = "";
-        return obj;
+
+        this.desc = "doSomething is a function that creates and return a new Calculator object instance that is initialized with value '0'. The calculator instance has 3 functions: add(num), sub(num) and getVale(). Each one of them changes the calculator instance own value";
     };
 
-    var objCreated = new ObjectFactory();
-    return objCreated;
+    return new ObjectConstructor();
 }
-
-function q4(){
-    //return a calculator object that has 3 methods add(num), sub(num) getValue() and no other exposed keys
-    //string: description of what doSomething() is doing
-    var ObjectConstructor = function(func, desc){
-        this.func = func;
-        this.desc = desc;
-    };
-
-    var calculator = function(){
-        this.num = 0;
-
-        var add = function(num){
-            this.num += num;
-        }
-
-        var sub = function(num){
-            this.num -= num;
-        }
-
-        var getValue = function(){
-            return this.num;
-        }
-    }
-
-    var objCreated = new ObjectConstructor(calculator, 'test');
-    return objCreated;
-}
-
-q1().doSomething();
