327f57697e6369a8cc94757a543f8ce0f232cfc3
Nitai <nitaiaharoni1@gmail.com>
2019-05-25 14:36:01 +0300

    finish?

--- Ex2/item3/test/calcExpress.test.js
+++ Ex2/item3/test/calcExpress.test.js
@@ -3,7 +3,7 @@ const assert = require('chai').assert,
     parallel = require('mocha.parallel'),
     app = require('../calcExpress');
 
-parallel('calcExpress Test', function(){
+describe('calcExpress Test', function(){
     let req = request(app);
 
     it('User1', async function(){
@@ -11,10 +11,25 @@ parallel('calcExpress Test', function(){
         let uniqustring = res.text;
 
         res = await request(app).post(`/calc/${uniqustring}/add/5`).expect(200);
-        assert(res.text, '5')
+        assert(res.text, '5');
 
-        res = await request(app).post(`/calc/${uniqustring}/sub/10`).expect(200)
-        assert(res.text, '-5')
+        res = await request(app).post(`/calc/${uniqustring}/sub/10`).expect(200);
+        assert(res.text, '-5');
+
+        res = await request(app).put(`/calc/${uniqustring}/multiply/-5`).expect(200);
+        assert(res.text, '25');
+
+        res = await request(app).put(`/calc/${uniqustring}/divide/5`).expect(200);
+        assert(res.text, '5');
+
+        res = await request(app).get(`/calc/${uniqustring}/M`).expect(200);
+        assert(res.text, '5');
+
+        res = await request(app).post(`/calc/${uniqustring}/reset`).expect(200);
+        assert(res.text, '0');
+
+        res = await request(app).delete(`/calc/${uniqustring}/del`).expect(200);
+        assert(res.text, 'Session was deleted!')
     });
 
     it('User2', async function(){
@@ -26,6 +41,21 @@ parallel('calcExpress Test', function(){
 
         res = await request(app).post(`/calc/${uniqustring}/sub/50`).expect(200);
         assert(res.text, '50');
+
+        res = await request(app).put(`/calc/${uniqustring}/divide/10`).expect(200);
+        assert(res.text, '5');
+
+        res = await request(app).put(`/calc/${uniqustring}/multiply/2`).expect(200);
+        assert(res.text, '10');
+
+        res = await request(app).get(`/calc/${uniqustring}/M`).expect(200);
+        assert(res.text, '10');
+
+        res = await request(app).post(`/calc/${uniqustring}/reset`).expect(200);
+        assert(res.text, '0');
+
+        res = await request(app).delete(`/calc/${uniqustring}/del`).expect(200);
+        assert(res.text, 'Session was deleted!')
     });
 
 });
