d7d16e428c4be2519bdf68f3f8557b9f36503f3f
I355539 <nitai.aharoni@sap.com>
2019-07-08 17:07:18 +0300

    init

--- store-project/src/App.js
+++ store-project/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import NavBar from './components/navbar'
+import Counters from './components/counters'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar/>
+                <main className="container">
+                    <Counters/>
+                </main>
+            </React.Fragment>
+        );
+    };
+};
